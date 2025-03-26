027a687574caa55d65bb29c047ed1e7fe63c22d3
Aquaday <martin991940@hotmail.com>
2025-03-26 12:18:22 +0100

    .

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 console.log('Hello!');
 
 let productArray = [
-    {Armchair}
+    {
+        furnitureImage: "images/orangearmchair.jpg",
+        name: "Orange Armchair",
+        info: "An orange armchair with matching footrest",
+        rating: "5/5",
+    },
+    {
+        furnitureImage: "images/brownchair.jpg",
+        name: "Brown Chair",
+        info: "A fancy but affordable chair",
+        rating: "5/5",
+    },
+    {
+        furnitureImage: "images/doublecouch.jpg",
+        name: "Double Couch",
+        info: "A durable, comfortable and affordable couch ",
+        rating: "5/5",
+    },
+    {
+        furnitureImage: "images/kitchen.jpg",
+        name: "Kitchen",
+        info: "We will help you build your dream kitchen",
+        rating: "5/5",
+    }
 ]
+
+
