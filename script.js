eb8f1f9e8406ae383b74b39ca0a69f1593d45251
Aquaday <martin991940@hotmail.com>
2025-04-19 03:47:12 +0200

    product Display Done

--- script.js
+++ script.js
@@ -24,34 +24,85 @@ let productArray = [
         name: "Kitchen",
         info: "We will help you build your dream kitchen",
         rating: "5/5",
+    },
+    {
+        furnitureImage: "images/Orangearmchair.jpg",
+        name: "Orange Armchair",
+        info: "An orange armchair with matching footrest",
+        rating: "5/5",
+    },
+    {
+        furnitureImage: "images/brownchair.jpg",
+        name: "Brown Chair",
+        info: "A fancy but affordable chair",
+        rating: "3/5",
+    },
+    {
+        furnitureImage: "images/doublecouch.jpg",
+        name: "Double Couch",
+        info: "A durable, comfortable and affordable couch ",
+        rating: "3/5",
+    },
+    {
+        furnitureImage: "images/kitchen.jpg",
+        name: "Kitchen",
+        info: "We will help you build your dream kitchen",
+        rating: "3/5",
+    },
+    {
+        furnitureImage: "images/Orangearmchair.jpg",
+        name: "Orange Armchair",
+        info: "An orange armchair with matching footrest",
+        rating: "1/5",
+    },
+    {
+        furnitureImage: "images/brownchair.jpg",
+        name: "Brown Chair",
+        info: "A fancy but affordable chair",
+        rating: "1/5",
+    },
+    {
+        furnitureImage: "images/doublecouch.jpg",
+        name: "Double Couch",
+        info: "A durable, comfortable and affordable couch ",
+        rating: "1/5",
+    },
+    {
+        furnitureImage: "images/kitchen.jpg",
+        name: "Kitchen",
+        info: "We will help you build your dream kitchen",
+        rating: "1/5",
+    },
+    {
+        furnitureImage: "images/brownchair.jpg",
+        name: "Brown Chair",
+        info: "A fancy but affordable chair",
+        rating: "1/5",
+    },
+    {
+        furnitureImage: "images/doublecouch.jpg",
+        name: "Double Couch",
+        info: "A durable, comfortable and affordable couch ",
+        rating: "1/5",
+    },
+    {
+        furnitureImage: "images/kitchen.jpg",
+        name: "Kitchen",
+        info: "We will help you build your dream kitchen",
+        rating: "1/5",
     }
 ]
 
+let arrayNumber = 0
 function placeElements() {
-    let arrayNumber = 0
+    console.log(arrayNumber + "pre loop")
     const productsPreviewCard = document.querySelector(".productsPreviewCards")
-    productArray.forEach((item) => {
+    productsPreviewCard.innerHTML = ""
+    for (let i = 0; i < 4; i++ ) {
         let divElement = document.createElement("div")
-        // let imgElement = document.createElement("div")
-        // let h3Element = document.createElement("h3")
-        // let h6Element = document.createElement("h6")
-        // let pElement = document.createElement("p")
-        // let buttonElement = document.createElement("button")
-        // imgElement.innerHTML = '<img src="'+ productArray[arrayNumber].furnitureImage+ '"width="100%"></img>' 
-        //       + '<h3>'+ productArray[arrayNumber].name + '</h3>'
-        // pElement.textContent = productArray[arrayNumber].info
-        // h6Element.textContent = productArray[arrayNumber].rating
-        // buttonElement.textContent = "Buy Now"
         divElement.classList.add("productsCard")
-
-        // divElement.appendChild(imgElement)
-        // divElement.appendChild(h3Element)
-        // divElement.appendChild(pElement)
-        // divElement.appendChild(h6Element)
-        // divElement.appendChild(buttonElement)
-        
         divElement.innerHTML = ' <img src="' + productArray[arrayNumber].furnitureImage + '" width="100%">' + 
-            '<div class="infoCardPlacement"> <div class="infoCard"> <h3 id="product1H3"> '
+            '<div class="infoCardPlacement"> <div class="infoCard"> <h3> '
              +  productArray[arrayNumber].name 
              + ' </h3>'
              + '<p>' + productArray[arrayNumber].info +'</p></div>'
@@ -59,18 +110,27 @@ function placeElements() {
              +  productArray[arrayNumber].rating + '</p> </div><div class="buttonCard"><button>Order now</button></div></div>'
              productsPreviewCard.appendChild(divElement)
         arrayNumber = arrayNumber + 1
+        console.log(arrayNumber + "for loop")
         
-    })
+    }
+    arrayNumber = arrayNumber - 4
+    console.log(arrayNumber + "post loop")
 }
 
 placeElements()
-
-let h3Element = document.createElement("h3")
-let pElement = document.createElement("p")
 
 let shiftLeft = document.querySelector("#shiftLeft")
 let shiftRight = document.querySelector("#shiftRight")
 
 shiftLeft.addEventListener("click", () => {
+    if (arrayNumber >= 1) {
+    arrayNumber -= 1
+    placeElements()
+}
+})
 
+shiftRight.addEventListener("click", () => {
+    if (arrayNumber <= productArray.length - 5) {
+    arrayNumber += 1
+    placeElements()}
 })
