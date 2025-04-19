d13e4fb2e9e409c121b2b9c20fbc490cc47b4aeb
Aquaday <martin991940@hotmail.com>
2025-04-19 03:23:17 +0200

    info from array

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('Hello!');
 
 let productArray = [
     {
-        furnitureImage: "images/orangearmchair.jpg",
+        furnitureImage: "images/Orangearmchair.jpg",
         name: "Orange Armchair",
         info: "An orange armchair with matching footrest",
         rating: "5/5",
@@ -28,10 +28,42 @@ let productArray = [
 ]
 
 function placeElements() {
+    let arrayNumber = 0
+    const productsPreviewCard = document.querySelector(".productsPreviewCards")
     productArray.forEach((item) => {
-        const 
+        let divElement = document.createElement("div")
+        // let imgElement = document.createElement("div")
+        // let h3Element = document.createElement("h3")
+        // let h6Element = document.createElement("h6")
+        // let pElement = document.createElement("p")
+        // let buttonElement = document.createElement("button")
+        // imgElement.innerHTML = '<img src="'+ productArray[arrayNumber].furnitureImage+ '"width="100%"></img>' 
+        //       + '<h3>'+ productArray[arrayNumber].name + '</h3>'
+        // pElement.textContent = productArray[arrayNumber].info
+        // h6Element.textContent = productArray[arrayNumber].rating
+        // buttonElement.textContent = "Buy Now"
+        divElement.classList.add("productsCard")
+
+        // divElement.appendChild(imgElement)
+        // divElement.appendChild(h3Element)
+        // divElement.appendChild(pElement)
+        // divElement.appendChild(h6Element)
+        // divElement.appendChild(buttonElement)
+        
+        divElement.innerHTML = ' <img src="' + productArray[arrayNumber].furnitureImage + '" width="100%">' + 
+            '<div class="infoCardPlacement"> <div class="infoCard"> <h3 id="product1H3"> '
+             +  productArray[arrayNumber].name 
+             + ' </h3>'
+             + '<p>' + productArray[arrayNumber].info +'</p></div>'
+             + '<div class="ratingCard"><p>' 
+             +  productArray[arrayNumber].rating + '</p> </div><div class="buttonCard"><button>Order now</button></div></div>'
+             productsPreviewCard.appendChild(divElement)
+        arrayNumber = arrayNumber + 1
+        
     })
 }
+
+placeElements()
 
 let h3Element = document.createElement("h3")
 let pElement = document.createElement("p")
